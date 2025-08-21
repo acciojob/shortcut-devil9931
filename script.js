836b0d32ebaf62a36b5a384dcc83660297fdb3e0
sumitrathore <114568634+devil9931@users.noreply.github.com>
2025-08-22 00:05:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function shortcut(s1, s2) {
   // your code here
+	if (!s1 || !s2) return '';
+  return s1[0] + s2[0];
 }
+
+console.log(shortcut("Amnesty", "International"));
+console.log(shortcut("Hello", "world"));
+console.log(shortcut("", "International"));
+console.log(shortcut("Amnesty", ""));
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
